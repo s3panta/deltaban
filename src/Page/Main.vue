f10cc58358c952bd6781ae05d580a157c2a8a410
nima abbasi <nimaab1992@gmail.com>
2021-01-25 23:01:09 +0400

    add slick slider

--- src/Page/Main.vue
+++ src/Page/Main.vue
@@ -51,99 +51,19 @@
 
             </div>
         </div>
+        <Slick/>
 
-
-        <div class="cards">
-            <div class="slider">
-                <VueSlickCarousel v-bind="settings">
-                    <div>
-                        <div class="img_box">
-                            <img src="../assets/maldive.jpg" alt="...">
-                            <div class="img_title">
-                                <p>Maldive</p>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div>
-                        <div class="img_box">
-                            <img src="../assets/istanbul.jpg" alt="...">
-                            <div class="img_title">
-                                <p>Italy</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="img_box">
-                            <img src="../assets/italy.jpg" alt="...">
-                            <div class="img_title">
-                                <p>Canada</p>
-                            </div>
-                        </div>
-                    </div>
-
-                </VueSlickCarousel>
-
-            </div>
-        </div>
     </div>
 </template>
 
 <script>
-    import VueSlickCarousel from 'vue-slick-carousel'
-    import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-    // optional style for arrows & dots
-    import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+    import Slick from "../components/Slider/Slick"
     export default {
         components: {
-            VueSlickCarousel
+            Slick
         },
 
-        data() {
-            return {
-                settings: {
-                    "dots": false,
-                    "arrows": false,
-                    "focusOnSelect": true,
-                    "infinite": true,
-                    "speed": 500,
-                    "slidesToShow": 2,
-                    "slidesToScroll": 3,
-                    "touchThreshold": 5,
-                    
-                    "centerPadding": "20px",
-                    "swipeToSlide": true,
-                    "rtl": false,
-                    "autoplay": false,
-                    "responsive": [{
-                            "breakpoint": 1024,
-                            "settings": {
-                                "slidesToShow": 2,
-                                "slidesToScroll": 3,
-                                "infinite": true,
-                                "dots": true
-                            }
-                        },
-                        {
-                            "breakpoint": 600,
-                            "settings": {
-                                "slidesToShow": 2,
-                                "slidesToScroll": 2,
-                                "initialSlide": 2
-                            }
-                        },
-                        {
-                            "breakpoint": 480,
-                            "settings": {
-                                "slidesToShow": 2,
-
-                            }
-                        }
-                    ]
-
-                },
-            }
-        },
+       
 
     }
 </script>
@@ -212,64 +132,4 @@ color: #004D40;
         background-color: #004D40 !important;
     }
 
-    .cards {
-        margin-top: 30px;
-        margin-bottom: 60px;
-        margin-left: 10px;
-    }
-
-    .img_box {
-        position: relative;
-
-
-    }
-
-    .img_box>img {
-        width: 200px;
-        height: 250px;
-        border-radius: 20px;
-    }
-
-    .img_title {
-        position: absolute;
-        top: 10px;
-        left: 5px;
-        color: #EEEEEE;
-    }
-                @media only screen and (max-width: 1940px) {
-        .img_box>img {
-            width: 450px;
-        height: 500px;
-
-        }
-    }
-            @media only screen and (max-width: 1024px) {
-        .img_box>img {
-            width: 350px;
-        height: 400px;
-
-        }
-    }
-        @media only screen and (max-width: 600px) {
-        .img_box>img {
-            width: 200px;
-        height: 250px;
-
-        }
-    }
-
-    @media only screen and (max-width: 400px) {
-        .img_box>img {
-            width: 170px;
-            height: 200px;
-
-        }
-    }
-        @media only screen and (max-width: 320px) {
-        .img_box>img {
-            width: 150px;
-            height: 200px;
-
-        }
-    }
 </style>
